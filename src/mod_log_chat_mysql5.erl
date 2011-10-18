68ea6a4eeee6d951ad249a9033ab793e5f9b6db4
Michael Weibel <michael.weibel@gmail.com>
2011-10-18 16:15:26 +0200

    adding gen_server behaviour to support graceful restart of db connection, if the connection is broken

--- src/mod_log_chat_mysql5.erl
+++ src/mod_log_chat_mysql5.erl
@@ -1,20 +1,24 @@
 %%%----------------------------------------------------------------------
 %%% File    : mod_log_chat_mysql5.erl
-%%% Author  : Jérôme Sautret <jerome.sautret@process-one.net>
 %%% Author  : Michael Weibel <michael.weibel@amiadogroup.com>
-%%% Purpose : Log chat messages in a mysql dbs
+%%% Original Author  : Jérôme Sautret <jerome.sautret@process-one.net>
+%%% Purpose : Log chat messages to a mysql db
 %%%----------------------------------------------------------------------
 
 -module(mod_log_chat_mysql5).
 -author('michael.weibel@amiadogroup.com').
 
 -behaviour(gen_mod).
+-behaviour(gen_server).
 
--export([start/2,
-	init/1,
+%% gen_mod callbacks
+-export([start/2, start_link/2,
 	stop/1,
-	log_packet_send/3,
-	handle_info/2, terminate/2]).
+	log_packet_send/3]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
 %%-define(ejabberd_debug, true).
 
@@ -23,21 +27,51 @@
 
 -define(PROCNAME, ?MODULE).
 
--record(state, {dbref, server, port, db, user, password}).
-
 table_name() ->
 	"mod_log_chat_mysql5".
 
+%% start db connection
+start_link(Host, Opts) ->
+	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	gen_server:start_link({local, Proc}, ?MODULE, [Host, Opts], []).
+
+%% Start module & start gen_server as a child for the db connection
 start(Host, Opts) ->
-	?INFO_MSG(" ~p  ~p~n", [Host, Opts]),
 	ejabberd_hooks:add(user_send_packet, Host, ?MODULE, log_packet_send, 55),
 	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
-	gen_server:start({local, Proc}, ?MODULE, [Opts], []).
 
-init([Opts]) ->
+	ChildSpec =
+		{Proc,
+			{?MODULE, start_link, [Host, Opts]},
+			permanent,
+			1000,
+			worker,
+			[?MODULE]},
+	supervisor:start_child(ejabberd_sup, ChildSpec).
+
+%% stop module (remove hooks) & stop gen server
+stop(Host) ->
+    ejabberd_hooks:delete(user_send_packet, Host,
+			  ?MODULE, log_packet_send, 55),
+	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	gen_server:call(Proc, stop),
+	supervisor:delete_child(ejabberd_sup, Proc).
+
+%% called from start_link/2 and sets up the db connection
+init([_Host, Opts]) ->
     ?INFO_MSG("Starting ~p", [?MODULE]),
 
 	crypto:start(),
+
+	LogFun = fun(debug, _Format, _Argument) ->
+	                %?DEBUG(Format, Argument);
+	                 ok;
+	               (error, Format, Argument) ->
+	                 ?INFO_MSG(Format, Argument);
+	               (Level, Format, Argument) ->
+	                 ?INFO_MSG("MySQL (~p)~n", [Level]),
+	                 ?INFO_MSG(Format, Argument)
+	            end,
 
 	Server = gen_mod:get_opt(server, Opts, "localhost"),
 	Port = gen_mod:get_opt(port, Opts, 3306),
@@ -45,64 +79,65 @@ init([Opts]) ->
 	User = gen_mod:get_opt(user, Opts, "root"),
 	Password = gen_mod:get_opt(password, Opts, ""),
 
-	St = #state{server=Server, port=Port, db=DB,
-	               user=User, password=Password},
+	?INFO_MSG("Opening mysql connection ~s@~s:~p/~s", [User, Server, Port, DB]),
+	mysql:start_link(mod_log_chat_mysql5_db, Server, Port, User, Password, DB, LogFun).
 
-	case open_mysql_connection(St) of
-	    {ok, DBRef} ->
-	        State = St#state{dbref=DBRef},
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	mysql:stop(),
+    ok.
 
-			ets:new(mod_log_chat_mysql5, [named_table, protected, set, {keypos, 1}]),
-			ets:insert(mod_log_chat_mysql5, {dbref, DBRef}),
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
-	        Result = create_table(State),
-			?INFO_MSG("create table: ~p", [Result]),
-	        erlang:monitor(process, DBRef),
-	        {ok, State};
-	    {error, Reason} ->
-	        ?ERROR_MSG("MySQL connection failed: ~p~n", [Reason]),
-	        {stop, db_connection_failed}
-	end.
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State}.
 
-open_mysql_connection(#state{server=Server, port=Port, db=DB,
-                             user=DBUser, password=Password} = _State) ->
-   LogFun = fun(debug, _Format, _Argument) ->
-                %?DEBUG(Format, Argument);
-                 ok;
-               (error, Format, Argument) ->
-                 ?ERROR_MSG(Format, Argument);
-               (Level, Format, Argument) ->
-                 ?DEBUG("MySQL (~p)~n", [Level]),
-                 ?DEBUG(Format, Argument)
-            end,
-   ?INFO_MSG("Opening mysql connection ~s@~s:~p/~s", [DBUser, Server, Port, DB]),
-   mysql_conn:start(Server, Port, DBUser, Password, DB, LogFun).
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast({insert_row, FromJid, ToJid, Body, Type}, State) ->
+	?INFO_MSG("inserting row: %p, %p, %s, %s", [FromJid, ToJid, Body, Type]),
+	Query = ["INSERT INTO ", table_name(), " (fromJid, toJid, sentDate, body, type) VALUES",
+	 		"(\"",
+	FromJid,
+	"\", \"",
+	ToJid, "\", NOW(), \"",
+	Body, "\", \"", Type ,"\");"],
 
-cleanup(Host) ->
-	close_mysql_connection(),
-    ejabberd_hooks:delete(user_send_packet, Host,
-			  ?MODULE, log_packet_send, 55),
-	ok.
+	sql_query(Query),
+	{noreply, State}.
 
-close_mysql_connection() ->
-	[{_, DBRef}] = ets:lookup(mod_log_chat_mysql5, dbref),
-	?DEBUG("Closing ~p mysql connection", [DBRef]),
-	mysql_conn:stop(DBRef).
-
-stop(Host) ->
-    cleanup(Host),
-	ok.
-
+%% handle module infos
 handle_info({'DOWN', _MonitorRef, process, _Pid, _Info}, State) ->
     {stop, connection_dropped, State};
 handle_info(Info, State) ->
     ?INFO_MSG("Got Info:~p, State:~p", [Info, State]),
     {noreply, State}.
 
-terminate(_Reason, #state{}=_State) ->
-	close_mysql_connection(),
-	ok.
-
+%% ejabberd hook
 log_packet_send(From, To, Packet) ->
     log_packet(From, To, Packet).
 
@@ -117,6 +152,7 @@ log_packet(From, To, Packet = {xmlelement, "message", Attrs, _Els}) ->
 log_packet(_From, _To, _Packet) ->
     ok.
 
+%% parse message and send to db connection gen_server
 write_packet(From, To, Packet, Type) ->
     Body = escape(html, xml:get_path_s(Packet, [{elem, "body"}, cdata])),
     case Body of
@@ -133,8 +169,14 @@ write_packet(From, To, Packet, Type) ->
 			true ->
 				ToJid = To#jid.luser++"@"++To#jid.lserver
 		end,
-		insert_row(FromJid, ToJid, Body, Type)
+		Proc = gen_mod:get_module_proc(From#jid.server, ?PROCNAME),
+		?INFO_MSG("Proc: ~p", [Proc]),
+		gen_server:cast(Proc, {insert_row, FromJid, ToJid, Body, Type})
     end.
+
+%% ==================
+%% SQL Query API
+%% ==================
 
 escape(text, Text) ->
     Text;
@@ -147,53 +189,17 @@ escape(html, [$& | Text]) ->
 escape(html, [Char | Text]) ->
     [Char | escape(html, Text)].
 
-
-create_table(#state{dbref=DBRef}) ->
-	?INFO_MSG("Mysql Conn.: ~p", [DBRef]),
-	Query =  ["CREATE TABLE IF NOT EXISTS ", table_name(), "(",
-                "id INT AUTO_INCREMENT PRIMARY KEY, ",
-                "fromJid VARCHAR(255) NOT NULL,",
-				"toJid VARCHAR(255) NOT NULL,",
-				"sentDate TIMESTAMP NOT NULL,",
-				"body TEXT,",
-				"type VARCHAR(10)",
-                ") ENGINE=MyISAM CHARACTER SET utf8"],
-	case sql_query_internal(DBRef, Query) of
-		{updated, _} -> ok;
-		{error, _Reason} -> error
-	end.
-
-insert_row(FromJid, ToJid, Body, Type) ->
-	[{_, DBRef}] = ets:lookup(mod_log_chat_mysql5, dbref),
-	Query = ["INSERT INTO ", table_name(), " (fromJid, toJid, sentDate, body, type) VALUES",
-	 		"(\"",
-	FromJid,
-	"\", \"",
-	ToJid, "\", NOW(), \"",
-	Body, "\", \"", Type ,"\");"],
-
-	case sql_query_internal(DBRef, Query) of
-		{updated, _} -> ok;
-		{error, _Reason} -> error
-	end.
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% SQL internals
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sql_query_internal(DBRef, Query) ->
-    case sql_query_internal_silent(DBRef, Query) of
+sql_query(Query) ->
+    case sql_query_internal_silent(Query) of
          {error, Reason} ->
-            ?ERROR_MSG("~p while ~p", [Reason, lists:append(Query)]),
+           ?INFO_MSG("~p while ~p", [Reason, lists:append(Query)]),
             {error, Reason};
          Rez -> Rez
     end.
 
-sql_query_internal_silent(DBRef, Query) ->
-    ?DEBUG("DOING: \"~s\"", [lists:append(Query)]),
-    get_result(mysql_conn:fetch(DBRef, Query, self())).
+sql_query_internal_silent(Query) ->
+    ?INFO_MSG("DOING: \"~s\"", [lists:append(Query)]),
+    get_result(mysql:fetch(mod_log_chat_mysql5_db, Query)).
 
 get_result({updated, MySQLRes}) ->
     {updated, mysql:get_result_affected_rows(MySQLRes)};
